54089b51a3a1eeaafb1bdb0d98bfe67f9a2d5549
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2024-12-15 15:08:52 +0530

    Initial Commit

---
+++ src/components/particles/Data.tsx
@@ -0,0 +1,55 @@
+export const NavLinks = [
+    {
+        name: "Home",
+        url: "/"
+    },
+    
+];
+
+
+
+export const HeroTexts = [
+    {
+        Heading: "Bhai Data.jsx me jaake edit kar",
+        Paragraph: "Welcome to GymNex, where fitness meets fun! Get ready to unleash your inner strength and transform your life!",
+        Button: "Join Now!"
+    },
+    {
+        Heading: "New Heights of Fitness!",
+        Paragraph: "Push your limits and achieve greatness at our gym! Join our supportive community and embark on a journey of self-improvement and empowerment!",
+        Button: "Sign Up Today!"
+    },
+    {
+        Heading: "Fitness is Fun with Us!",
+        Paragraph: "At our gym, fitness is not just a routine; it's an enjoyable adventure! Join us in this fitness expedition, where fun and results go hand in hand!",
+        Button: "Enroll Now!"
+    }
+]
+
+
+export const AboutTexts = {
+    firstText: "About GymNex",
+    secondText: "Welcome",
+    caption: "Welcome to GymNex, where fitness meets community and transformation happens!",
+    paragraph1: "At GymNex, we are more than just a gym; we are a passionate and supportive fitness family dedicated to helping you achieve your health and wellness goals. Our mission is to create a positive and empowering environment that inspires you to challenge yourself, embrace a healthy lifestyle, and discover the best version of yourself.",
+}
+
+export const OfferTexts = {
+    firstText: "What we do",
+    secondText: "All Our Offer",
+    list: [
+        {
+            listCaption: "Weight Loose Programs",
+            text: "Our personalized approach, expert guidance, and proven results will help you achieve your fitness goals. Take the first step towards a transformed body and lifestyle today.",
+        },
+        {
+            listCaption: "Body Building Programs",
+            text: "Our expert trainers will guide you through tailored workouts, helping you achieve your bodybuilding goals efficiently and safely.",
+        }
+        , {
+            listCaption: "Different Special Classes",
+            text: "Take your workout to new heights with our high and intense Special Class, where you'll push your limits and achieve remarkable fitness results.",
+        }
+    ],
+}
+
